1aed4f0874742c22440c11b8bd002b9a645c93b9
Spencer Carli <spencercarli@gmail.com>
2019-04-02 10:35:31 -0500

    Eslint and prettier

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default class App extends React.Component {
   render() {
     return (
@@ -10,12 +19,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
