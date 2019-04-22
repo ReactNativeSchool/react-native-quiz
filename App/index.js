961eeb850613014826dbac0494e380c682a741bc
Spencer Carli <spencercarli@gmail.com>
2019-04-22 10:51:44 -0500

    remove code for recording

--- App/index.js
+++ App/index.js
@@ -1,26 +1,8 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from "react";
+import { View, Text } from "react-native";
 
-import Quiz from "./screens/Quiz";
-import QuizIndex from "./screens/QuizIndex";
-
-const MainStack = createStackNavigator({
-  QuizIndex: {
-    screen: QuizIndex,
-    navigationOptions: {
-      headerTitle: "Quizzes"
-    }
-  },
-  Quiz: {
-    screen: Quiz,
-    navigationOptions: ({ navigation }) => ({
-      headerTitle: navigation.getParam("title"),
-      headerTintColor: "#fff",
-      headerStyle: {
-        backgroundColor: navigation.getParam("color"),
-        borderBottomColor: navigation.getParam("color")
-      }
-    })
-  }
-});
-
-export default createAppContainer(MainStack);
+export default () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Future home of a quiz app!</Text>
+  </View>
+);
